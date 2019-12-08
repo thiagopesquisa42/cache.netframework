cf2299664a96bf2ef279b47ef92c795b9f9c083b
Thiago Vieira Reginaldo <thiagopesquisa42@gmail.com>
2019-12-08 01:38:12 -0300

    Adicionado teste simples demontrativo do funcionamento da cache.

--- src/MemoryCache/Console/Program.cs
+++ src/MemoryCache/Console/Program.cs
@@ -1,11 +1,27 @@
-﻿namespace Console
+﻿using static MemoryCacheCustom.MemoryCacheCustom;
+
+namespace Console
 {
     class Program
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Testando...");
-            System.Console.Read();
+            var valor = ObterItemCache(42, TestFunction);
+            System.Console.WriteLine($"{valor.GetType().Name}: {valor}");
+            valor = ObterItemCache(42, TestFunction);
+            System.Console.WriteLine($"{valor.GetType().Name}: {valor}");
+            valor = ObterAtualizarItemCache(42, TestFunction);
+            System.Console.WriteLine($"{valor.GetType().Name}: {valor}");
+            valor = ObterItemCache(42, TestFunction);
+            System.Console.WriteLine($"{valor.GetType().Name}: {valor}");
+            System.Console.ReadLine();
+        }
+
+        static int TestFunction()
+        {
+            System.Console.WriteLine("TestFunction Executed");
+            var random = new System.Random();
+            return random.Next();
         }
     }
 }
